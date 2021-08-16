b1df359a5580b407c5b33372285e9a6855e15027
Sam <api.tester.acc003@gmail.com>
2021-08-17 03:18:06 +0530

    completed till chapter 12 - arrays & Collection

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -26,8 +26,38 @@ namespace Test
             //answer = num1 + num2;
             //Console.WriteLine("The Answer is: " + answer);
 
+            //Defining an array ---- Fixed Type & Fixed Length
+                int[] intArray = new int[5];
+                string[] populatedStringArray = new string[] { "Hello", "I", "am", "Sam" };
+
+                int[,] multiIntArray = new int[2, 3];
+                string[,] multiPopulatedStringArray = { { "Jammu & Kashmir", "Ladakh", "Punjab" }, { "Srinagar", "Leh", "Chandigarh"} };
+
+            //Collections ------- Lists - Fixed Type & Variable Length (Numeric)
+                List<String> listOfStrings = new List<String>();
+                listOfStrings.Add("Hi");
+                listOfStrings.Add("This");
+                listOfStrings.Add("Is");
+                listOfStrings.Add("Sam");
+
+                listOfStrings.Insert(3, "Mr.");     //Inserts "Mr." before "Sam"
+            
+                listOfStrings.Remove("Hi");
+                //Or
+                listOfStrings.RemoveAt(0);          //Removes whatever is there at index 0
+
+                listOfStrings.Sort();               //Ascending Sort
+
+            //Collection -------- Dictionary - Fixed Type & Variable Length (Associative)
+            Dictionary<string, string> names = new Dictionary<string, string>();
+                names.Add("first_name", "Sam");
+                names.Add("last_name", "Sahoo");
+
+                names.Remove("last_name");
+
+
             //User Defined Variables -----------------------------------------
-            PetStruct dog;
+            PetStruct dog = new PetStruct();
             dog.Type = PetType.Dog;
             dog.HasFur = true;
             dog.Legs = 4;
@@ -37,11 +67,22 @@ namespace Test
             duck.HasFur = false;
             duck.Legs = 2;
 
-            Console.Write("a " + dog.Type + " has " + dog.Legs + " Legs");
-            Console.Write("a " + duck.Type + " has " + duck.Legs + " Legs");
+            Console.WriteLine("a " + dog.Type + " has " + dog.Legs + " Legs");
+            Console.WriteLine("a " + duck.Type + " has " + duck.Legs + " Legs");
+
+            MultipleLegs(dog, duck);
+
             Console.ReadLine();
         }
 
+        static void MultipleLegs(PetStruct petStruct, PetClass petClass)
+        {
+            petStruct.Legs = petStruct.Legs * 2;
+            petClass.Legs = petClass.Legs * 2;
+
+            Console.WriteLine("Internal Method - A " + petStruct.Type + " has " + petStruct.Legs + " Legs.");
+            Console.WriteLine("Internal Method - A " + petClass.Type + " has " + petClass.Legs + " Legs.");
+        }
     }
 
     class PetClass
