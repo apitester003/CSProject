7244ba1fa3a1a0fb2b6297da2f0629d37b2b8e08
Samarendra Sahoo <ssahoo@milestechnologies.com>
2021-08-24 17:19:51 -0400

    commit dated 20210825

--- Playground/Playground/Program.cs
+++ Playground/Playground/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyLibrary;
 
 namespace Playground
 {
@@ -10,7 +11,10 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            MyObject myObject = new MyObject();
+            MyObject myObject = new MyObject(3, 5);
+
+            com.squareconnection.mylibrary.MyObject myNewObject = new com.squareconnection.mylibrary.MyObject(4, 5);
+
             myObject.number1 = 10;
             myObject.number2 = 15;
             Console.WriteLine(myObject.Calculate());
@@ -18,60 +22,5 @@ namespace Playground
         }
     }
 
-    class MyObject
-    {
-        public MyObject(int number1, int number2)       //Constructor
-        {
-            _number1 = number1;
-            _number2 = number2;
-        }
-
-        int _number1 = 0; //these are Intenal variables, we dont need to get/set 
-        int _number2 = 0; //these are Intenal variables, we dont need to get/set 
-        int _number3 = 0; //these are Intenal variables, we dont need to get/set 
-        //public int number1 { get; set; }
-        public int number1
-        {
-            get
-            {
-               return _number1;
-            }
-            set
-            {
-                _number1 = value;
-            }
-        }
-        //public int number2 { get; set; }
-        public int number2
-        {
-            get
-            {
-                return _number2;
-            }
-            set
-            {
-                _number2 = value;
-            }
-        }
-
-        public int number3
-        {
-            get
-            {
-                return _number1 + _number2;
-            }
-            set
-            {
-                _number3 = value;
-            }
-        }
-        public int Calculate() {
-            return number1 + number2;
-        }
-
-        public string MyMethod()
-        {
-            return "Some Value";
-        }
-    }
+    
 }
---
+++ Playground/com.squareconnection.mylibrary/MyObject.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.squareconnection.mylibrary
+{
+    public class MyObject
+    {
+        public MyObject(int number1, int number2)       //Constructor
+        {
+            _number1 = number1;
+            _number2 = number2;
+        }
+
+        int _number1 = 0; //these are Intenal variables, we dont need to get/set 
+        int _number2 = 0; //these are Intenal variables, we dont need to get/set 
+        int _number3 = 0; //these are Intenal variables, we dont need to get/set 
+        //public int number1 { get; set; }
+        public int number1
+        {
+            get
+            {
+                return _number1;
+            }
+            set
+            {
+                _number1 = value;
+            }
+        }
+        //public int number2 { get; set; }
+        public int number2
+        {
+            get
+            {
+                return _number2;
+            }
+            set
+            {
+                _number2 = value;
+            }
+        }
+
+        public int number3
+        {
+            get
+            {
+                return _number1 + _number2;
+            }
+            set
+            {
+                _number3 = value;
+            }
+        }
+        public int Calculate()
+        {
+            return number1 + number2;
+        }
+
+        public string MyMethod()
+        {
+            return "Some Value";
+        }
+    }
+}
